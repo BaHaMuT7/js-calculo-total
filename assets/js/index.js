922c0b22e70dccec56b1b48c4c0d3e8f4fffe8d7
Raúl Pardo Zurita <raul.pardoz@gmail.com>
2022-06-16 11:51:44 -0400

    Se terminan estilos de tarjeta y componentes internos

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+import Utilities from "./Utilities.js";
+import utilities from "./Utilities.js";
+
+
+Utilities.applyStyleSpanTag();
+Utilities.applyStyleArticleTag();
+Utilities.applyStyleInputTag();
+Utilities.applyStyleImgTag();
+Utilities.applyStyleh2Tag();
+utilities.applyStyleButtonTag();
+
