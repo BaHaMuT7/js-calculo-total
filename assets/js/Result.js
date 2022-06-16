ccf467fbd276cdbaf95d40dd6d59cf3671d4ae33
Raúl Pardo Zurita <raul.pardoz@gmail.com>
2022-06-16 17:12:04 -0400

    Se termina bloque de resultados
    Se termina bloque de resultados y con ello, todos los bloques desde el DOM

--- assets/js/Result.js
+++ assets/js/Result.js
@@ -1,5 +1,78 @@
 export class Result {
 
+    //Componentes
+    static result = document.querySelector(".resultados");
+    static circle = document.querySelector(".circulo");
+    static results = document.querySelectorAll(".resultados");
+    static innerFlex = document.querySelectorAll(".total, .cantidad, .color");
+
+    //Estilos
+
+    //Result Block Styles
+    static result_marginLeft = "50px";
+
+    //Results inner three (total, cantidad y color)
+    static innerFlex_display = "flex";
+
+    //Results span
+    static span_marginTop = "50px";
+    static span_marginLeft = "10px";
+    static span_fontSize = "50px";
+
+    //Results h3
+    static h3_fontSize = "50px";
+
+    //Results circle
+    static circle_marginLeft = "10px";
+    static circle_marginTop = "50px";
+    static circle_height = "50px";
+    static circle_width = "50px";
+    static circle_backgroundColor = "#FFF";
+    static circle_borderRadius = "50px";
+    static circle_border = "1px solid black";
+
+
+    static applyStyleResultBlock = () => {
+       Result.result.style.marginLeft = Result.result_marginLeft;
+    }
+
+    static applyStyleInnerFlexBlocks = () => {
+        Result.innerFlex.forEach(element => {
+            element.style.display = Result.innerFlex_display;
+        });
+    }
+
+    static applyStyleSpanTag = () => {
+        Result.results.forEach(element => {
+            let tagArray = element.getElementsByTagName("span");
+            for (let tag of tagArray) {
+                tag.style.marginTop = Result.span_marginTop;
+                tag.style.marginLeft = Result.span_marginLeft;
+                tag.style.fontSize = Result.span_fontSize;
+            }
+        });
+    }
+
+    static applyStyleH3Tag = () => {
+        Result.results.forEach(element => {
+            let tagArray = element.getElementsByTagName("h3");
+            for (let tag of tagArray) {
+                tag.style.fontSize = Result.h3_fontSize;
+            }
+        });
+    }
+
+    static applyStyleCircleBlock = () => {
+        Result.circle.style.marginLeft = Result.circle_marginLeft;
+        Result.circle.style.marginTop = Result.circle_marginTop;
+        Result.circle.style.height = Result.circle_height;
+        Result.circle.style.width = Result.circle_width;
+        Result.circle.style.backgroundColor = Result.circle_backgroundColor;
+        Result.circle.style.borderRadius = Result.circle_borderRadius;
+        Result.circle.style.border = Result.circle_border;
+    }
+
+
 }
 
 export default Result;
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,17 @@ import Cards from "./Cards.js";
 import Container from "./Container.js";
 import Result from "./Result.js";
 
+//Main container styles are applied
 Container.applyStyleContainerBlock();
+
+//Styles are applied over the result block
+Result.applyStyleResultBlock();
+Result.applyStyleInnerFlexBlocks();
+Result.applyStyleSpanTag();
+Result.applyStyleH3Tag();
+Result.applyStyleCircleBlock();
+
+//Styles are applied over cards
 Cards.applyStyleSpanTag();
 Cards.applyStyleArticleTag();
 Cards.applyStyleInputTag();
