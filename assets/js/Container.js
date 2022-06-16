b105663b897a8bd24924d685f5ec705235db7a2d
Raúl Pardo Zurita <raul.pardoz@gmail.com>
2022-06-16 15:52:43 -0400

    Se termina estilo del contenedor
    Se termina estilo del contenedor y se diseña estilo para resultados

--- assets/js/Container.js
+++ assets/js/Container.js
@@ -1,5 +1,17 @@
 export class Container {
 
+    //Componentes
+    static container = document.querySelector(".container");
+
+    //Estilos
+    static container_display = "flex";
+    static container_alignItems = "center";
+
+    static applyStyleContainerBlock = () => {
+        Container.container.style.display = Container.container_display;
+        Container.container.style.alignItems = Container.container_alignItems;
+    }
+
 }
 
 export default Container;
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ import Cards from "./Cards.js";
 import Container from "./Container.js";
 import Result from "./Result.js";
 
-
+Container.applyStyleContainerBlock();
 Cards.applyStyleSpanTag();
 Cards.applyStyleArticleTag();
 Cards.applyStyleInputTag();
